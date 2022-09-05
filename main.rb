a2cf260052d4511b2e2f893957f1f50913321fdc
Mondre <engelbrechtmondre@gmail.com>
2022-09-05 13:32:24 +0800

    Add colorize gem and test it out

--- main.rb
+++ main.rb
@@ -1,5 +1,19 @@
+require 'colorize'
+# One parent class: Mastermind. Two child classes: Codebreaker and codemaker.
 class Mastermind
-    def initialize
-        @numbers = [1, 2, 3, 4, 5, 6]
-    end
+  def initialize
+    @pegs = { 1 => :blue, 2 => :red, 3 => :yellow, 4 => :magenta, 5 => :cyan, 6 => :green }
+  end
+  
+  #puts String.colors
+  #puts String.modes
+  #puts String.color_samples
+  puts 'Hello world'.on_blue
+  puts 'Hello world'.on_red
+  puts 'Hello world'.on_yellow
+  puts 'Hello world'.on_green
+  puts 'Hello world'.on_magenta
+  puts 'Hello world'.on_cyan
 end
+
+Mastermind.new
